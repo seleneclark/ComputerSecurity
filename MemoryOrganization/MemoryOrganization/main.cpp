bea5e5d52513a925542366d46b50b16a97851dc8
Selene Clark <michellc@byui.edu>
2021-10-25 22:00:02 -0600

    Starting Memory Organization project

---
+++ MemoryOrganization/MemoryOrganization/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MemoryOrganization
+//
+//  Created by Selene on 10/25/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
