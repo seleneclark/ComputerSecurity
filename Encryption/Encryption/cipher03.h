4be38e28c8be2d22494d731d8f36febb3d3d1308
Michael Jackson <emjaypower@byui.edu>
2021-12-10 14:26:01 -0800

    implemented affine cipher in Cipher03.h

--- Encryption/Encryption/cipher03.h
+++ Encryption/Encryption/cipher03.h
@@ -5,68 +5,153 @@
 ********************************************************************/
 #ifndef CIPHER03_H
 #define CIPHER03_H
-
+#include <iostream>
 /********************************************************************
  * CLASS
  *******************************************************************/
 class Cipher03 : public Cipher
 {
 public:
-   virtual std::string getPseudoAuth()  { return "pseudocode author"; }
-   virtual std::string getCipherName()  { return "cipher name"; }
-   virtual std::string getEncryptAuth() { return "encrypt author"; }
-   virtual std::string getDecryptAuth() { return "decrypt author"; }
+    Cipher03()
+    {
+        valueMinimum = ' ';
+        valueMaximum = '~';
+        sizeAlphabet = valueMaximum - valueMinimum + 1;
+    }
+    virtual std::string getPseudoAuth() { return "Michael Jackson"; }
+    virtual std::string getCipherName() { return "Affine Cipher"; }
+    virtual std::string getEncryptAuth() { return "Michael Jackson"; }
+    virtual std::string getDecryptAuth() { return "Michael Jackson"; }
 
-   /***********************************************************
-    * GET CIPHER CITATION
-    * Returns the citation from which we learned about the cipher
-    ***********************************************************/
-   virtual std::string getCipherCitation()
-   {
-      return std::string("citation");
-   }
-   
-   /**********************************************************
-    * GET PSEUDOCODE
-    * Returns the pseudocode as a string to be used by the caller.
-    **********************************************************/
-   virtual std::string getPseudocode()
-   {
-      std::string str;
+    /***********************************************************
+     * GET CIPHER CITATION
+     * Returns the citation from which we learned about the cipher
+     ***********************************************************/
+    virtual std::string getCipherCitation()
+    {
+        std::string s;
+        s += "dcode.fr (2021), ";
+        s += "\"Affine Cipher - Online Decryption, Encoder, Decoder, Calculator\", \n Retrieved: ";
+        s += "https://www.dcode.fr/affine-cipher";
+        return s;
+    }
 
-      // TODO: please format your pseudocode
-      // The encrypt pseudocode
-      str =  "insert the encryption pseudocode\n";
+    /**********************************************************
+     * GET PSEUDOCODE
+     * Returns the pseudocode as a string to be used by the caller.
+     **********************************************************/
+    virtual std::string getPseudocode()
+    {
+        std::string str;
 
-      // The decrypt pseudocode
-      str += "insert the decryption pseudocode\n";
+        // TODO: please format your pseudocode
+        // The encrypt pseudocode
+        str = "encrypt(plaintext, password)\n";
+        str += "   a <- indexFromCharacter(password[0]) % sizeAlphabet\n";
+        str += "   b <- indexFromCharacter(password[1]) % sizeAlphabet\n";
+        str += "   a <- indexFromCharacter(password[0]) % sizeAlphabet\n";
+        str += "   FOR p is all values of plainText\n";
+        str += "      y <- (a * indexFromCharacter(*p) + b) % size\n";
+        str += "      cipherText += characterFromIndex(y)\n";
+        str += "   RETURN cipherText\n\n";
 
-      return str;
-   }
+        // The decrypt pseudocode
+        str += "decrypt(cipherText, password)\n";
+        str += "   a <- indexFromCharacter(password[0]) % sizeAlphabet\n";
+        str += "   b <- indexFromCharacter(password[1]) % sizeAlphabet\n";
+        str += "   FOR p is all values of cipherText\n";
+        str += "      x <- (modInverse(a) * (indexFromCharacter(*p) - b)) % size\n";
+        str += "      plainText += characterFromIndex(x)\n";
+        str += "   RETURN plainText\n\n";
 
-   /**********************************************************
-    * ENCRYPT
-    * TODO: ADD description
-    **********************************************************/
-   virtual std::string encrypt(const std::string & plainText,
-                               const std::string & password)
-   {
-      std::string cipherText = plainText;
-      // TODO - Add your code here
-      return cipherText;
-   }
+        return str;
+    }
 
-   /**********************************************************
-    * DECRYPT
-    * TODO: ADD description
-    **********************************************************/
-   virtual std::string decrypt(const std::string & cipherText,
-                               const std::string & password)
-   {
-      std::string plainText = cipherText;
-      // TODO - Add your code here
-      return plainText;
-   }
+    /**********************************************************
+     * ENCRYPT
+     * TODO: ADD description
+     **********************************************************/
+    virtual std::string encrypt(const std::string& plainText,
+        const std::string& password)
+    {
+        std::string cipherText;
+        // TODO - Add your code here
+        int a = indexFromCharacter(password[0]) % sizeAlphabet;
+        int b = indexFromCharacter(password[1]) % sizeAlphabet;
+
+        for (const char* p = plainText.c_str(); *p; p++)
+        {
+            // affine equation y = (ax+b) % sizeAlphabet
+            int x = indexFromCharacter(*p);
+            int y = (a * x + b) % sizeAlphabet;
+            cipherText += characterFromIndex(y);
+        }
+        return cipherText;
+    }
+
+    /**********************************************************
+     * DECRYPT
+     * TODO: ADD description
+     **********************************************************/
+    virtual std::string decrypt(const std::string& cipherText,
+        const std::string& password)
+    {
+        std::string plainText;
+        // TODO - Add your code here
+        int a = indexFromCharacter(password[0]) % sizeAlphabet;
+        int b = indexFromCharacter(password[1]) % sizeAlphabet;
+
+        for (const char* p = cipherText.c_str(); *p; p++)
+        {
+            // affine equation x = a'(y - b) % sizeAlphabet
+            int y = indexFromCharacter(*p);
+            int A = calcModularInverse(a);
+            int x = ((y - b) * A) % sizeAlphabet;
+            if (x < 0)
+                x += sizeAlphabet;
+            plainText += characterFromIndex(x);
+        }
+        return plainText;
+    }
+private:
+    /**************************************************
+    * INDEX FROM CHARACTER
+    * Get an index value from a given letter.
+    * Author: Bro. Helfrich
+    *************************************************/
+    int indexFromCharacter(char letter)
+    {
+        // return the value
+        if (letter > valueMaximum || letter < valueMinimum)
+            return 0;
+        else
+            return (int)(letter - valueMinimum);
+    }
+
+    /**************************************************
+     * CHARACTER FROM INDEX
+     * Get the characer value from a given index.
+     * Author: Bro. Helfrich
+     *************************************************/
+    char characterFromIndex(int index)
+    {
+        if (index >= 0 && index < sizeAlphabet)
+            return (char)(index + valueMinimum);
+        else
+            return ' ';
+    }
+
+    // Get the Modular inverse of a value such that (a * A') % sizeAlphabet == 1
+    int calcModularInverse(int a)
+    {
+        for (int x = 0; x <= sizeAlphabet; x++)
+            if ((a * x) % sizeAlphabet == 1)
+                return x;
+    }
+
+    char valueMinimum;
+    char valueMaximum;
+    int sizeAlphabet;
 };
 
 #endif // CIPHER03_H
